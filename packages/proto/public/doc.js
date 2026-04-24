902d10e1980ee62571117c694f41621f6a9c882c
Dakshesh <dpasala@calpoly.edu>
2026-04-24 16:38:09 -0700

    aight looks good for lab 7

---
+++ packages/proto/public/doc.js
@@ -0,0 +1,17 @@
+document.body.addEventListener(
+    "dark-mode:toggle",
+    (event) => {
+        const page = event.currentTarget;
+        const checked = event.detail.checked;
+        page.classList.toggle("dark-mode", checked);
+    }
+);
+
+function toggleDarkMode(target, checked) {
+    const customEvent = new CustomEvent(
+        "dark-mode:toggle", {
+            bubbles: true,
+            detail: { checked },
+        }
+    );
+}
