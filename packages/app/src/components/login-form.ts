13aca4d1d14cab3ff6302029dbf82be637470c4f
Dakshesh <dpasala@calpoly.edu>
2026-06-05 12:21:02 -0700

    added just some interface stuff to username password and error because it was annoying how if i did something wrong on the login/register forms, it didn't really show anything, so i wanted to add that

--- packages/app/src/components/login-form.ts
+++ packages/app/src/components/login-form.ts
@@ -2,19 +2,27 @@ import { css, html, shadow } from "@unbndl/html";
 import { createViewModel, fromInputs } from "@unbndl/view";
 import reset from "./reset.css.js";
 
+interface LoginFormViewModel {
+  username: string;
+  password: string;
+  error?: string;
+}
+
 export class LoginFormElement extends HTMLElement {
-  viewModel = createViewModel({
+  viewModel = createViewModel<LoginFormViewModel>({
     username: "",
-    password: ""
+    password: "",
+    error: undefined
   }).with(
     fromInputs<{ username: string; password: string }>(this),
     "username",
     "password"
   );
 
-  view = html<[{ username: string; password: string }]>
+  view = html<[LoginFormViewModel]>
     `<form>
       <slot></slot>
+      ${($) => $.error ? html`<p class="error">${$.error}</p>` : ""}
       <button type="submit">
         <slot name="submit-label">Login</slot>
       </button>
@@ -39,13 +47,30 @@ export class LoginFormElement extends HTMLElement {
     };
     const body = JSON.stringify(data);
     console.log("Posting login form:", endpoint, body, event);
+    this.viewModel.set("error", undefined);
+
     fetch(endpoint, { method, headers, body })
         .then((res) => {
-        if (res.status !== 200 && res.status !== 201)
-            throw `Form submission failed: Status ${res.status}`;
+        if (res.status !== 200 && res.status !== 201) {
+            if (endpoint.includes("register") && res.status === 409) {
+                this.viewModel.set("error", "Username exists");
+                return undefined;
+            }
+
+            if (endpoint.includes("login") && res.status === 401) {
+                this.viewModel.set("error", "Invalid username or password");
+                return undefined;
+            }
+
+            this.viewModel.set("error", `Form submission failed: Status ${res.status}`);
+            return undefined;
+        }
+        
         return res.json();
         })
         .then((json) => {
+        if (!json) return;
+
         const { token } = json;
         const customEvent = new CustomEvent("auth:message", {
             bubbles: true,
